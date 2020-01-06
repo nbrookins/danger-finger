5420863b04b0e3fedda5f2fc1cba84651479f973
Nicholas Brookins <nbrookins@DangerBox.local>
2020-01-05 20:45:43 -0800

    refactors

--- danger_finger.py
+++ danger_finger.py
@@ -454,12 +454,11 @@ class DangerFinger:
             if iterable(mod_segment):
                 if self.rotate != 0: mod_segment = [rotate(self.rotate_offsets[p][i])(x) for i, x in enumerate(mod_segment, 0)]
                 if self.explode: mod_segment = [translate(self.explode_offsets[p][i])(x) for i, x in enumerate(mod_segment, 0)]
-                mod_segment = mod_segment = [translate(self.translate_offsets[p][i])(x) for i, x in enumerate(mod_segment, 0)]
+                mod_segment = [translate(self.translate_offsets[p][i])(x) for i, x in enumerate(mod_segment, 0)]
             else:
                 if self.explode: mod_segment = mod_segment.translate(self.explode_offsets[p])
                 #print(self.rotate_offsets, self.rotate_offsets[p])
                 r = max(self._rotate_offsets[p], key=lambda v: v)
-                print(r)
                 if r != 0: mod_segment = mod_segment.rotate(self.rotate_offsets[p])
                 if r > 2: mod_segment = mod_segment.rotate(self.rotate_offsets[p])
                 mod_segment = mod_segment.translate(self.translate_offsets[p])
@@ -481,66 +480,32 @@ class DangerFinger:
     @property
     def explode_offsets(self):
         ''' amount to expand during explode'''
-        offs = self._explode_offsets
-        new_offs = {}
-        f = self._explode_factor
-        for o in offs:
-            if isinstance(offs[o][0], (int, float)):
-                new_offs[o] = (offs[o][0] * f, offs[o][1] * f, offs[o][2] * f)
-            else:
-                new_sub = []
-                for v in offs[o]:
-                    new_sub.append((v[0] * f * self.rotate, v[1] * f, v[2] * f))
-                new_offs[o] = new_sub
-        return new_offs
-
+        return self._prop_offset(self._explode_offsets, self._explode_factor)
     _explode_offsets = { #TODO - make these parametric
-        "middle":(0, 20, 0), "base" : (0, 0, 0), "tip":(0, 40, 0),
-        "socket":(0, -20, 0), "linkage" : (10, -30, 0), "tipcover" : (0, 50, 0),
-        "plugs":((0, 0, -8), (0, 0, 8), (0, 40, -8), (0, 40, 8))}
+        "middle":(0, 20, 0), "base" : (0, 0, 0), "tip":(0, 40, 0), "socket":(0, -20, 0),
+        "linkage" : (10, -30, 0), "tipcover" : (0, 50, 0), "plugs":((0, 0, -8), (0, 0, 8), (0, 40, -8), (0, 40, 8))}
 
     @property
     def translate_offsets(self):
         ''' amount to expand during explode'''
-        offs = self._translate_offsets
-        new_offs = {}
-        f = self.distal_offset
-        for o in offs:
-            if isinstance(offs[o][0], (int, float)):
-                new_offs[o] = (offs[o][0] * f, offs[o][1] * f, offs[o][2] * f)
-            else:
-                new_sub = []
-                for v in offs[o]:
-                    new_sub.append((v[0] * f, v[1] * f, v[2] * f))
-                new_offs[o] = new_sub
-        return new_offs
-
+        return self._prop_offset(self._translate_offsets, self.distal_offset)
     _translate_offsets = {
-        "middle":(0, 0, 0), "base" : (0, 0, 0), "tip":(0, 1, 0),
-        "socket":(0, 0, 0), "linkage" : (0, 0, 0), "tipcover" : (0, 0, 0),
-        "plugs":((0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0))}
+        "middle":(0, 0, 0), "base" : (0, 0, 0), "tip":(0, 1, 0), "socket":(0, 0, 0),
+        "linkage" : (0, 0, 0), "tipcover" : (0, 0, 0), "plugs":((0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0))}
 
     @property
     def rotate_offsets(self):
         ''' amount to expand during rotate'''
-        offs = self._rotate_offsets
-        new_offs = {}
-        f = self._rotate_factor * self.rotate
-        #print(f, self.rotate)
-        for o in offs:
-            if isinstance(offs[o][0], (int, float)):
-                new_offs[o] = (min((offs[o][0], 1)) * f, min((offs[o][1], 1)) * f, min((offs[o][2], 1)) * f)
-            else:
-                new_sub = []
-                for v in offs[o]:
-                    new_sub.append((min(v[0], 1) * f, min(v[1], 1) * f, min(v[2], 1) * f))
-                new_offs[o] = new_sub
-        return new_offs
-
+        return self._prop_offset(self._rotate_offsets, self._rotate_factor * self.rotate, func=lambda v: min(v,1))
     _rotate_offsets = {
-        "middle":(0, 0, 1), "base" : (0, 0, 0), "tip":(0, 0, 3),
-        "socket":(0, 0, 0), "linkage" : (0, 0, 0), "tipcover" : (0, 0, 0),
-        "plugs":((0, 0, 0), (0, 0, 0), (0, 0, 2), (0, 0, 2))}
+        "middle":(0, 0, 1), "base" : (0, 0, 0), "tip":(0, 0, 3), "socket":(0, 0, 0),
+        "linkage" : (0, 0, 0), "tipcover" : (0, 0, 0), "plugs":((0, 0, 0), (0, 0, 0), (0, 0, 2), (0, 0, 2))}
+
+    def _prop_offset(self, offs, f, func=lambda v: v):
+        ''' calculate offsets by facot for animation, etc '''
+        new_offs = {o: (func(offs[o][0]) * f, func(offs[o][1]) * f, func(offs[o][2]) * f) if isinstance(offs[o][0], (int, float)) else \
+            [(func(v[0]) * f, func(v[1]) * f, func(v[2]) * f) for v in offs[o]] for o in offs}
+        return new_offs
 
     _part = []
     @property
@@ -553,27 +518,13 @@ class DangerFinger:
 
     #The minimum circumferential length of a polygon segment.  higher is faster
     fs = property(lambda self: ({
-        RenderQuality.ULTRAHIGH : .1,
-        RenderQuality.HIGH : .2,
-        RenderQuality.EXTRAMEDIUM : .3,
-        RenderQuality.MEDIUM : .4,
-        RenderQuality.SUBMEDIUM : .6,
-        RenderQuality.FAST : 1,
-        RenderQuality.ULTRAFAST : 1.5,
-        RenderQuality.AUTO : 1 if self.preview else .2
-        }[self.render_quality]))
+        RenderQuality.ULTRAHIGH : .1, RenderQuality.HIGH : .2, RenderQuality.EXTRAMEDIUM : .3, RenderQuality.MEDIUM : .4, RenderQuality.SUBMEDIUM : .6,
+        RenderQuality.FAST : 1, RenderQuality.ULTRAFAST : 1.5, RenderQuality.AUTO : 1 if self.preview else .2}[self.render_quality]))
 
     #the minimum dgrees of each polygon framgment , higher is faster
     fa = property(lambda self: ({
-        RenderQuality.ULTRAHIGH : 1.5,
-        RenderQuality.HIGH : 2,
-        RenderQuality.EXTRAMEDIUM : 3,
-        RenderQuality.MEDIUM : 4,
-        RenderQuality.SUBMEDIUM : 5,
-        RenderQuality.FAST : 6,
-        RenderQuality.ULTRAFAST : 10,
-        RenderQuality.AUTO : 6 if self.preview else 2
-        }[self.render_quality]))
+        RenderQuality.ULTRAHIGH : 1.5, RenderQuality.HIGH : 2, RenderQuality.EXTRAMEDIUM : 3, RenderQuality.MEDIUM : 4, RenderQuality.SUBMEDIUM : 5,
+        RenderQuality.FAST : 6, RenderQuality.ULTRAFAST : 10, RenderQuality.AUTO : 6 if self.preview else 2}[self.render_quality]))
 
     scad_header = property(lambda self: ("$fa = %s; \n$fs = %s;\n" % (self.fa, self.fs)))
 
